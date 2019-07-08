3a680565b5d1c0cf0000fa040c457d2d6ba64f34
Siby Mariam <52189788+sibymariam@users.noreply.github.com>
2019-07-08 19:12:09 +0530

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,1 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in= new Scanner(System.in);
+		int a=in.nextInt();
+		if(a==0)
+	   {
+	   	System.out.println("Zero");
+	   }
+	   else if(a<0)
+	   {
+	   	System.out.println("Negative");
+	   }
+	   else
+	   {
+	   	System.out.println("Positive");
+	   }
+	}
+}
