c7f0e610f1f09f04fccfc936b145d0aed48fb1d4
Siby Mariam <52189788+sibymariam@users.noreply.github.com>
2019-07-08 20:35:26 +0530

    Update voco.py

--- voco.py
+++ voco.py
@@ -1,5 +1,8 @@
-ch = input()
-if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
-	print('Vowel')
+ch=input("")
+if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+        print("Vowel")
+    else:
+        print("Consonant")
 else:
-	print('Consonant')
+print("invalid")
