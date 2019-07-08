1152e1769f420cc11c69afd24012c6e519f58b13
Siby Mariam <52189788+sibymariam@users.noreply.github.com>
2019-07-08 19:25:00 +0530

    Update zero.py

--- zero.py
+++ zero.py
@@ -1,13 +1,8 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
+import java.util.Scanner;
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner in= new Scanner(System.in);
 		int a=in.nextInt();
 		if(a==0)
