1cf32467eb1e80ef09bb748a2ca8596613ea73ff
Siby Mariam <52189788+sibymariam@users.noreply.github.com>
2019-07-19 23:44:43 +0530

    Update Asum.py

--- Asum.py
+++ Asum.py
@@ -1,3 +1,10 @@
-a = [int(i) for i in input().split()]
-b = [int(i) for i in input().split()]
-print(b[0]+b[1])
+N,K=map(int,input().split())
+arr=[]
+arr=[int(i) for i in input().split()]
+r=0
+n=0
+while K>0:
+	n+=arr[r]
+	K=K-1
+	r=r+1
+print(n)
